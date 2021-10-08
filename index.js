45a1005d576d6188a5e3c0b097d22a3c59a8dee2
Heavytik <37910484+Heavytik@users.noreply.github.com>
2021-10-08 14:50:09 +0300

    lisää cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors())
 
 function arvoNumero(alaraja, ylaraja) {
     return Math.floor((ylaraja - alaraja) * Math.random()) + alaraja
